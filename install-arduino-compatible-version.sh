6c3d6bd8eed2fa6ff1ff96187473c97320a45cef
Konrad Scorciapino <konr@konr.mobi>
2016-01-09 23:24:09 -0200

    minor fixes

--- install-arduino-compatible-version.sh
+++ install-arduino-compatible-version.sh
@@ -3,7 +3,7 @@
 target_script=install.sh
 target_script_fullpath=$(echo $(dirname $0) / $target_script)
 
-export $BREWPI_SCRIPT_TAG=0.4.0
-export $BREWPI_WWW_TAG=0.4.0
+export BREWPI_SCRIPT_TAG=0.4.0
+export BREWPI_WWW_TAG=0.4.0
 
 $target_script_fullpath
