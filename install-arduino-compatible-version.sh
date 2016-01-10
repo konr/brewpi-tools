d4d53a31db111e7957736b95d9d6eacc5d79b7e9
Konrad Scorciapino <konr@konr.mobi>
2016-01-09 23:22:54 -0200

    wrapper

---
+++ install-arduino-compatible-version.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+target_script=install.sh
+target_script_fullpath=$(echo $(dirname $0) / $target_script)
+
+export $BREWPI_SCRIPT_TAG=0.4.0
+export $BREWPI_WWW_TAG=0.4.0
+
+$target_script_fullpath
