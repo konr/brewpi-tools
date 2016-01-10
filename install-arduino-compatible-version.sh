d7267e12528cd8e17e597d8af9b97912d57d572f
Konrad Scorciapino <konr@konr.mobi>
2016-01-09 23:29:12 -0200

    moar fixes

--- install-arduino-compatible-version.sh
+++ install-arduino-compatible-version.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 target_script=install.sh
-target_script_fullpath=$(echo $(dirname $0) / $target_script)
+dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+target_script_fullpath="$dir/$target_script"
 
 export BREWPI_SCRIPT_TAG=0.4.0
 export BREWPI_WWW_TAG=0.4.0
